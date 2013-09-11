686730a0b817f787f11c9501373d8f7dd8dbd162
eddiefisher <eddifisher@bk.ru>
2013-09-12 00:05:03 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewsMetastudio::Application.config.secret_key_base = 'e5bdd91a1ec438dbdb98f9015150b8a6a7e6ef4a4307d4fc6f512f2538b40b38ead42b85c3e5a858da53821c0653ec9eafe41d2ccc9f34d2a33a51d185a035b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewsMetastudio::Application.config.session_store :cookie_store, key: '_news_metastudio_session'
